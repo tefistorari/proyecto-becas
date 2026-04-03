0ed29d9bb37afb5d3b7c55246cc630aac5a16d15
tefistorari <estefaniastorari14@gmail.com>
2026-04-03 18:23:01 -0300

    primer commit backend

---
+++ Sistema_Becas/src/main/java/com/UTN_BECAS/Sistema_Becas/SistemaBecasApplication.java
@@ -0,0 +1,13 @@
+package com.UTN_BECAS.Sistema_Becas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBecasApplication.class, args);
+	}
+
+}
---
+++ Sistema_Becas/src/test/java/com/UTN_BECAS/Sistema_Becas/SistemaBecasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UTN_BECAS.Sistema_Becas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
