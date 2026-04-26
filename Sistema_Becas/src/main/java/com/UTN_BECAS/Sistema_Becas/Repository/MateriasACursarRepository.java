301f4034c2c59ba8ef9360e5d49ca6aca39ce466
tefistorari <estefaniastorari14@gmail.com>
2026-04-26 02:25:06 -0300

    Update MateriasACursarRepository.java

--- Sistema_Becas/src/main/java/com/UTN_BECAS/Sistema_Becas/Repository/MateriasACursarRepository.java
+++ Sistema_Becas/src/main/java/com/UTN_BECAS/Sistema_Becas/Repository/MateriasACursarRepository.java
@@ -1,8 +1,9 @@
 package com.UTN_BECAS.Sistema_Becas.Repository;
 
+import com.UTN_BECAS.Sistema_Becas.Model.MateriasACursar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MateriasACursar extends JpaRepository<MateriasACursar, Long> {
+public interface MateriasACursarRepository extends JpaRepository<MateriasACursar, Long> {
 }
