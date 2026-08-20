af74e1cf47d25a9e404438b9b22189cb785d4c06
Estefania Storari <estefi1114@gmail.com>
2026-08-20 16:12:15 -0300

    Actualizacion pequeña
    Se agrego el id de convocatorias al link y modifico el html en relacion

--- frontend/src/app/pages/alumno/dashboard-alumno/dashboard-alumno.ts
+++ frontend/src/app/pages/alumno/dashboard-alumno/dashboard-alumno.ts
@@ -4,6 +4,7 @@ import { Router, RouterLink } from '@angular/router';
 import { ConvocatoriaService } from '../../../services/convocatoria-service';
 import { ConvocatoriaResponse } from '../../../models/convocatoria-response';
 import { DatePipe } from '@angular/common';
+import { TipoBeca } from '../../../models/tipo-beca';
 
 @Component({
   selector: 'app-dashboard-alumno',
@@ -39,6 +40,23 @@ export class DashboardAlumno implements OnInit{
           );
         }
       });
+  }
+
+  irAFormulario(convocatoria: ConvocatoriaResponse): void {
+    switch(convocatoria.beca.tipoBeca) {
+
+      case TipoBeca.BASE:
+        this.router.navigate(['/alumno/postular/base', convocatoria.id]);
+        break;
+
+      case TipoBeca.BIS:
+        this.router.navigate(['alumno/postular/bis', convocatoria.id]);
+        break;
+
+      case TipoBeca.BINID:
+        this.router.navigate(['alumno/postular/binid', convocatoria.id]);
+        break;
+    }
   }
 
   confirmLogout(): void {
