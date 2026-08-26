473ce084c46f94762e24d14e07681f8a64f246a0
Estefania Storari <estefi1114@gmail.com>
2026-08-26 15:23:36 -0300

    fix(dashboardadmin): Se venm correctamente los datos de ubiicacion al abrir un perfil.

--- frontend/src/app/pages/admin/dashboard-admin/dashboard-admin.ts
+++ frontend/src/app/pages/admin/dashboard-admin/dashboard-admin.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { PerfilService } from '../../../services/perfil-service';
 import { UsuarioResponse } from '../../../models/usuario-response';
 import { DatosPersonalesResponse } from '../../../models/datos-personales-response';
+import { UbicacionService } from '../../../services/ubicacion-service';
 
 @Component({
   selector: 'app-dashboard-admin',
@@ -15,18 +16,25 @@ import { DatosPersonalesResponse } from '../../../models/datos-personales-respon
 export class DashboardAdmin {
   protected authService = inject(AuthService);
   private perfilService = inject(PerfilService);
+  private ubicacionService = inject(UbicacionService);
   private router = inject(Router);
 
   showLogoutModal = signal(false);
 
   // BUSCADOR DE ALUMNOS
-
   textoBusqueda = '';
 
   alumnosEncontrados = signal<UsuarioResponse[]>([]);
   buscando = signal(false);
   perfilSeleccionado = signal<DatosPersonalesResponse | null>(null);
   alumnoSeleccionado = signal<UsuarioResponse | null>(null);
+
+  nacionalidadNombre = signal('');
+  provinciaNombre = signal('');
+  localidadNombre = signal('');
+
+  provinciaFamiliarNombre = signal('');
+  localidadFamiliarNombre = signal('');
 
   buscarAlumnos(): void {
     
@@ -53,25 +61,88 @@ export class DashboardAdmin {
   }
 
   verPerfil(alumno: UsuarioResponse): void {
+    
+    this.nacionalidadNombre.set('');
+    this.provinciaNombre.set('');
+    this.localidadNombre.set('');
+    this.provinciaFamiliarNombre.set('');
+    this.localidadFamiliarNombre.set('');
+
     this.perfilService.buscarPorUsuarioId(alumno.id).subscribe({
-      next: (perfil) => {
+      next: async (perfil) => {
+        
         this.alumnoSeleccionado.set(alumno);
         this.perfilSeleccionado.set(perfil);
-      },
-      error: (error) => {
-        console.error('Error al obtener el perfil:', error);
 
+        const nacionalidades = await this.ubicacionService.getNacionalidades();
+        const provincias = await this.ubicacionService.getProvincias();
+
+        const nacionalidad = nacionalidades.find(
+          n => n.iso2 === perfil.nacionalidad
+        );
+        const provincia = provincias.find(
+          n => n.iso2 === perfil.provincia
+        );
+
+        this.nacionalidadNombre.set(nacionalidad?.name ?? perfil.nacionalidad);
+        this.provinciaNombre.set(provincia?.name ?? perfil.provincia);
+
+        if(perfil.provincia && perfil.localidad) {
+          const localidades = await this.ubicacionService.getLocalidades(
+            perfil.provincia
+          );
+
+          const localidad = localidades.find(
+            l => String(l.id) === String(perfil.localidad)
+          );
+          this.localidadNombre.set(localidad?.name ?? String(perfil.localidad));
+        }
+
+        //domicilio familiar
+        if(perfil.domicilioFamiliarDistinto && perfil.domicilioFamiliarProvincia) {
+          const provinciaFamiliar = provincias.find(
+            p => p.iso2 === perfil.domicilioFamiliarProvincia
+          );
+
+          this.provinciaFamiliarNombre.set(
+            provinciaFamiliar?.name ?? perfil.domicilioFamiliarProvincia
+          );
+
+          if(perfil.domicilioFamiliarLocalidad) {
+            const localidadesFamiliar = await this.ubicacionService.getLocalidades(
+              perfil.domicilioFamiliarProvincia
+            );
+
+            const localidadFamiliar = localidadesFamiliar.find(
+              l => String(l.id) === String(perfil.domicilioFamiliarLocalidad)
+            );
+
+            this.localidadFamiliarNombre.set(
+              localidadFamiliar?.name ?? 
+              String(perfil.domicilioFamiliarLocalidad)
+            );
+          }
+        }
+      },
+      error:(error) => {
+        console.error('Error al obtener el perfil:', error);
         if(error.status === 404) {
           this.alumnoSeleccionado.set(alumno);
           this.perfilSeleccionado.set(null);
         }
       }
-    })
+    });
   }
 
   cerrarPerfil(): void {
     this.perfilSeleccionado.set(null);
     this.alumnoSeleccionado.set(null);
+
+    this.nacionalidadNombre.set('');
+    this.provinciaNombre.set('');
+    this.localidadNombre.set('');
+    this.provinciaFamiliarNombre.set('');
+    this.localidadFamiliarNombre.set('');
   }
 
   // CERRAR SESION
